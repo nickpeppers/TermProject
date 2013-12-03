d4f21aca908077afc4ef0a4fa40b5e82b9bb0741
Nick Peppers <nick.peppers@hitcents.com>
2013-12-03 11:37:48 -0600

    Added numbers along axis

--- TermProject/Resources/Resource.designer.cs
+++ TermProject/Resources/Resource.designer.cs
@@ -60,188 +60,212 @@ namespace TermProject
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05003c
-			public const int ComputerStartButon = 2131034172;
+			// aapt resource value: 0x7f050044
+			public const int ComputerStartButon = 2131034180;
 			
-			// aapt resource value: 0x7f05003b
-			public const int PlayerStartButton = 2131034171;
+			// aapt resource value: 0x7f050043
+			public const int PlayerStartButton = 2131034179;
 			
-			// aapt resource value: 0x7f05003a
-			public const int TitleText = 2131034170;
-			
-			// aapt resource value: 0x7f050001
-			public const int button1 = 2131034113;
-			
-			// aapt resource value: 0x7f05000b
-			public const int button10 = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int button11 = 2131034124;
-			
-			// aapt resource value: 0x7f05000d
-			public const int button12 = 2131034125;
-			
-			// aapt resource value: 0x7f05000e
-			public const int button13 = 2131034126;
-			
-			// aapt resource value: 0x7f05000f
-			public const int button14 = 2131034127;
-			
-			// aapt resource value: 0x7f050011
-			public const int button15 = 2131034129;
-			
-			// aapt resource value: 0x7f050012
-			public const int button16 = 2131034130;
-			
-			// aapt resource value: 0x7f050013
-			public const int button17 = 2131034131;
-			
-			// aapt resource value: 0x7f050014
-			public const int button18 = 2131034132;
-			
-			// aapt resource value: 0x7f050015
-			public const int button19 = 2131034133;
-			
-			// aapt resource value: 0x7f050002
-			public const int button2 = 2131034114;
-			
-			// aapt resource value: 0x7f050016
-			public const int button20 = 2131034134;
-			
-			// aapt resource value: 0x7f050017
-			public const int button21 = 2131034135;
-			
-			// aapt resource value: 0x7f050019
-			public const int button22 = 2131034137;
-			
-			// aapt resource value: 0x7f05001a
-			public const int button23 = 2131034138;
-			
-			// aapt resource value: 0x7f05001b
-			public const int button24 = 2131034139;
-			
-			// aapt resource value: 0x7f05001c
-			public const int button25 = 2131034140;
-			
-			// aapt resource value: 0x7f05001d
-			public const int button26 = 2131034141;
-			
-			// aapt resource value: 0x7f05001e
-			public const int button27 = 2131034142;
-			
-			// aapt resource value: 0x7f05001f
-			public const int button28 = 2131034143;
-			
-			// aapt resource value: 0x7f050021
-			public const int button29 = 2131034145;
-			
-			// aapt resource value: 0x7f050003
-			public const int button3 = 2131034115;
-			
-			// aapt resource value: 0x7f050022
-			public const int button30 = 2131034146;
-			
-			// aapt resource value: 0x7f050023
-			public const int button31 = 2131034147;
-			
-			// aapt resource value: 0x7f050024
-			public const int button32 = 2131034148;
-			
-			// aapt resource value: 0x7f050025
-			public const int button33 = 2131034149;
-			
-			// aapt resource value: 0x7f050026
-			public const int button34 = 2131034150;
-			
-			// aapt resource value: 0x7f050027
-			public const int button35 = 2131034151;
-			
-			// aapt resource value: 0x7f050029
-			public const int button36 = 2131034153;
-			
-			// aapt resource value: 0x7f05002a
-			public const int button37 = 2131034154;
-			
-			// aapt resource value: 0x7f05002b
-			public const int button38 = 2131034155;
-			
-			// aapt resource value: 0x7f05002c
-			public const int button39 = 2131034156;
-			
-			// aapt resource value: 0x7f050004
-			public const int button4 = 2131034116;
-			
-			// aapt resource value: 0x7f05002d
-			public const int button40 = 2131034157;
-			
-			// aapt resource value: 0x7f05002e
-			public const int button41 = 2131034158;
-			
-			// aapt resource value: 0x7f05002f
-			public const int button42 = 2131034159;
-			
-			// aapt resource value: 0x7f050031
-			public const int button43 = 2131034161;
-			
-			// aapt resource value: 0x7f050032
-			public const int button44 = 2131034162;
-			
-			// aapt resource value: 0x7f050033
-			public const int button45 = 2131034163;
-			
-			// aapt resource value: 0x7f050034
-			public const int button46 = 2131034164;
-			
-			// aapt resource value: 0x7f050035
-			public const int button47 = 2131034165;
-			
-			// aapt resource value: 0x7f050036
-			public const int button48 = 2131034166;
-			
-			// aapt resource value: 0x7f050037
-			public const int button49 = 2131034167;
-			
-			// aapt resource value: 0x7f050005
-			public const int button5 = 2131034117;
-			
-			// aapt resource value: 0x7f050006
-			public const int button6 = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int button7 = 2131034119;
+			// aapt resource value: 0x7f050042
+			public const int TitleText = 2131034178;
 			
 			// aapt resource value: 0x7f050009
-			public const int button8 = 2131034121;
+			public const int button1 = 2131034121;
+			
+			// aapt resource value: 0x7f050013
+			public const int button10 = 2131034131;
+			
+			// aapt resource value: 0x7f050014
+			public const int button11 = 2131034132;
+			
+			// aapt resource value: 0x7f050015
+			public const int button12 = 2131034133;
+			
+			// aapt resource value: 0x7f050016
+			public const int button13 = 2131034134;
+			
+			// aapt resource value: 0x7f050017
+			public const int button14 = 2131034135;
+			
+			// aapt resource value: 0x7f050019
+			public const int button15 = 2131034137;
+			
+			// aapt resource value: 0x7f05001a
+			public const int button16 = 2131034138;
+			
+			// aapt resource value: 0x7f05001b
+			public const int button17 = 2131034139;
+			
+			// aapt resource value: 0x7f05001c
+			public const int button18 = 2131034140;
+			
+			// aapt resource value: 0x7f05001d
+			public const int button19 = 2131034141;
 			
 			// aapt resource value: 0x7f05000a
-			public const int button9 = 2131034122;
+			public const int button2 = 2131034122;
+			
+			// aapt resource value: 0x7f05001e
+			public const int button20 = 2131034142;
+			
+			// aapt resource value: 0x7f05001f
+			public const int button21 = 2131034143;
+			
+			// aapt resource value: 0x7f050021
+			public const int button22 = 2131034145;
+			
+			// aapt resource value: 0x7f050022
+			public const int button23 = 2131034146;
+			
+			// aapt resource value: 0x7f050023
+			public const int button24 = 2131034147;
+			
+			// aapt resource value: 0x7f050024
+			public const int button25 = 2131034148;
+			
+			// aapt resource value: 0x7f050025
+			public const int button26 = 2131034149;
+			
+			// aapt resource value: 0x7f050026
+			public const int button27 = 2131034150;
+			
+			// aapt resource value: 0x7f050027
+			public const int button28 = 2131034151;
+			
+			// aapt resource value: 0x7f050029
+			public const int button29 = 2131034153;
+			
+			// aapt resource value: 0x7f05000b
+			public const int button3 = 2131034123;
+			
+			// aapt resource value: 0x7f05002a
+			public const int button30 = 2131034154;
+			
+			// aapt resource value: 0x7f05002b
+			public const int button31 = 2131034155;
+			
+			// aapt resource value: 0x7f05002c
+			public const int button32 = 2131034156;
+			
+			// aapt resource value: 0x7f05002d
+			public const int button33 = 2131034157;
+			
+			// aapt resource value: 0x7f05002e
+			public const int button34 = 2131034158;
+			
+			// aapt resource value: 0x7f05002f
+			public const int button35 = 2131034159;
+			
+			// aapt resource value: 0x7f050031
+			public const int button36 = 2131034161;
+			
+			// aapt resource value: 0x7f050032
+			public const int button37 = 2131034162;
+			
+			// aapt resource value: 0x7f050033
+			public const int button38 = 2131034163;
+			
+			// aapt resource value: 0x7f050034
+			public const int button39 = 2131034164;
+			
+			// aapt resource value: 0x7f05000c
+			public const int button4 = 2131034124;
+			
+			// aapt resource value: 0x7f050035
+			public const int button40 = 2131034165;
+			
+			// aapt resource value: 0x7f050036
+			public const int button41 = 2131034166;
+			
+			// aapt resource value: 0x7f050037
+			public const int button42 = 2131034167;
 			
 			// aapt resource value: 0x7f050039
-			public const int linearLayout = 2131034169;
+			public const int button43 = 2131034169;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f05003a
+			public const int button44 = 2131034170;
+			
+			// aapt resource value: 0x7f05003b
+			public const int button45 = 2131034171;
+			
+			// aapt resource value: 0x7f05003c
+			public const int button46 = 2131034172;
+			
+			// aapt resource value: 0x7f05003d
+			public const int button47 = 2131034173;
+			
+			// aapt resource value: 0x7f05003e
+			public const int button48 = 2131034174;
+			
+			// aapt resource value: 0x7f05003f
+			public const int button49 = 2131034175;
+			
+			// aapt resource value: 0x7f05000d
+			public const int button5 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int button6 = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int button7 = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int button8 = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int button9 = 2131034130;
+			
+			// aapt resource value: 0x7f050041
+			public const int linearLayout = 2131034177;
 			
 			// aapt resource value: 0x7f050008
-			public const int linearLayout2 = 2131034120;
+			public const int linearLayout1 = 2131034120;
 			
 			// aapt resource value: 0x7f050010
-			public const int linearLayout3 = 2131034128;
+			public const int linearLayout2 = 2131034128;
 			
 			// aapt resource value: 0x7f050018
-			public const int linearLayout4 = 2131034136;
+			public const int linearLayout3 = 2131034136;
 			
 			// aapt resource value: 0x7f050020
-			public const int linearLayout5 = 2131034144;
+			public const int linearLayout4 = 2131034144;
 			
 			// aapt resource value: 0x7f050028
-			public const int linearLayout6 = 2131034152;
+			public const int linearLayout5 = 2131034152;
 			
 			// aapt resource value: 0x7f050030
-			public const int linearLayout7 = 2131034160;
+			public const int linearLayout6 = 2131034160;
 			
 			// aapt resource value: 0x7f050038
-			public const int relativeLayout = 2131034168;
+			public const int linearLayout7 = 2131034168;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout8 = 2131034112;
+			
+			// aapt resource value: 0x7f050040
+			public const int relativeLayout = 2131034176;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView3 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView4 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView5 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView6 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView7 = 2131034119;
 			
 			static Id()
 			{
